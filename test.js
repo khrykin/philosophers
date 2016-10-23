98ce3115e2a01b08b3fd2c0e2cd8c7ad3cd3ec2b
Dmitry Khrykin <khrykin@me.com>
2016-10-23 23:50:50 +0300

    .npmignore added

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 const assert = require('assert');
 const philosophers = require('./philosophers');
 
-process.stdout.write('Testing...\n');
+console.log('Testing...');
 process.stdout.write("should be accessable with require");
   assert(philosophers.length > 0);
   assert(typeof philosophers[0].name === 'string');
   assert(typeof philosophers[0].picture === 'string');
-  process.stdout.write(" ✓\n");
+  console.log(" ✓");
 
-process.stdout.write("Test passes.");
+console.log("Test passes!");
